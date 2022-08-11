f3610ce46598ccb25150cbb59d531a97da164082
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-08-11 13:21:06 +0600

    Html: element added

---
+++ FrontEnd/script.js
@@ -0,0 +1,6 @@
+function submit1(){
+    var a =  document.getElementById("number1").value;
+    var b =  document.getElementById("number2").value;
+    var c = parseInt(a) + parseInt(b); 
+    document.getElementById("screen1").innerHTML=c;
+  }
